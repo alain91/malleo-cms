bde799331ff0e1caf0a4188aa37d3023d5e1dc06
alain091@gmail.com <alain091@gmail.com@642bd02c-d575-5741-bdde-6fea00f7f757>
2011-03-25 18:58:28 +0000

    refactoring module citations

--- trunk/plugins/modules/citations/mod.php
+++ trunk/plugins/modules/citations/mod.php
@@ -25,7 +25,7 @@ $id_version = 0;
 
 global $root;
 
-require_once($root.'/class/class_controller.php');
+require_once(dirname(__FILE__).'/class_controller.php');
 
 class controller_citations extends Controller
 {
