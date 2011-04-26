129cb40e0527e41bb9fa42d585d513854844e570
alain091@gmail.com <alain091@gmail.com@642bd02c-d575-5741-bdde-6fea00f7f757>
2011-04-26 09:12:16 +0000

    version travail

--- plugins/modules/citations/actions/action_editer.php
+++ plugins/modules/citations/actions/action_editer.php
@@ -1,9 +1,19 @@
 <?php
-
 defined('CITATIONS_PATH') OR die("Tentative de Hacking");
 
 class action_editer extends Action
 {
+	function init()
+	{
+		global $droits,$module;
+		
+		if (!$droits->check($module,0,'poster'))
+		{
+			error404(518);
+			exit;
+		}
+	}
+	
 	function run()
 	{
 		global $session,$tpl,$droits,$module,$img,$lang,$root,$cf,$cache;
